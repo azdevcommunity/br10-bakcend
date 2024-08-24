a2d9376b08c0da3f522857e3c44c8de37e8ef2dd
Yusif Hasanov <yusif.dev.hasanov@gmail.com>
2024-08-25 01:53:29 +0400

    Update WebConfig.java

--- src/main/java/fib/br10/configuration/WebConfig.java
+++ src/main/java/fib/br10/configuration/WebConfig.java
@@ -31,7 +31,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/api/uploads/")
+        registry.addResourceHandler("/demo/uploads/**")
                 .addResourceLocations("file:/app/uploads/");
     }
 }
