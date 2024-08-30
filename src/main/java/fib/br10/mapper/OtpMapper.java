0b726b6bc356ad65f0dcdd81e92b35b429e34033
Yusif Hasanov <yusif.dev.hasanov@gmail.com>
2024-08-30 10:48:45 +0400

    change

---
+++ src/main/java/fib/br10/mapper/OtpMapper.java
@@ -0,0 +1,9 @@
+package fib.br10.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE, unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface OtpMapper {
+}
