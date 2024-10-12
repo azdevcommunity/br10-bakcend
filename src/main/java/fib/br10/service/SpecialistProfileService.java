5792d85f510fdbdc890f1d2406656950cf0c3594
Yusif Hasanov <yusif.dev.hasanov@gmail.com>
2024-10-12 04:24:49 +0400

    Update SpecialistProfileService.java

--- src/main/java/fib/br10/service/SpecialistProfileService.java
+++ src/main/java/fib/br10/service/SpecialistProfileService.java
@@ -12,6 +12,7 @@ import fib.br10.dto.specialist.specialistprofile.response.SpecialistProfileReadR
 import fib.br10.entity.Image;
 import fib.br10.entity.specialist.SpecialistProfile;
 import fib.br10.entity.specialist.Speciality;
+import fib.br10.entity.user.User;
 import fib.br10.exception.specialist.specialistprofile.SpecialistProfileAlreadyExists;
 import fib.br10.exception.specialist.specialistprofile.SpecialistProfileNotFoundException;
 import fib.br10.mapper.SpecialistProfileMapper;
@@ -48,12 +49,13 @@ public class SpecialistProfileService {
     ImageService imageService;
     RequestContextProvider provider;
 
-    @Cacheable(value = SPECIALIST_PROFILE, key = "#id")
+//    @Cacheable(value = SPECIALIST_PROFILE, key = "#id")
     public SpecialistProfileReadResponse read(Long id) {
         //TODO: refactor this select image with join
         SpecialistProfile specialistProfile = findBySpecialistUserId(id);
         SpecialistProfileReadResponse response = specialistProfileMapper.specialistProfileToSpecialistProfileResponse(specialistProfile);
-
+        User user = userService.findById(specialistProfile.getSpecialistUserId());
+        response.setUserName(user.getUsername());
         if (Objects.nonNull(specialistProfile.getImageId())) {
             Image image = imageService.findById(specialistProfile.getImageId());
             if (Objects.nonNull(image))
