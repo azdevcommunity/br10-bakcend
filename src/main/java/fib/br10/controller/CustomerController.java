996158863444ca337a66f6477b70163c25469bcb
Maqsud Safin <communityazdev@gmail.com>
2025-01-30 16:29:47 +0400

    customer history added

--- src/main/java/fib/br10/controller/CustomerController.java
+++ src/main/java/fib/br10/controller/CustomerController.java
@@ -6,7 +6,6 @@ import java.util.List;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
